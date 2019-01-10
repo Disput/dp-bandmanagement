4e54adb26952a709720f2b7a36975367703c1ba6
Alexander Haase <alexander.haase@de.ibm.com>
2019-01-10 15:09:20 +0100

    init maven project default

---
+++ src/main/java/dp/bandmanagement/App.java
@@ -0,0 +1,13 @@
+package dp.bandmanagement;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
